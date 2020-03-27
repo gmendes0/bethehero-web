5cdfc3541eed172e8c90a15dbd752ab48939d6a3
gmendes0 <gmendes230@gmail.com>
2020-03-27 13:36:15 -0300

    feat: added profile page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 export default function Routes() {
   return (
@@ -13,6 +14,9 @@ export default function Routes() {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/profile">
+          <Profile />
         </Route>
       </Switch>
     </BrowserRouter>
